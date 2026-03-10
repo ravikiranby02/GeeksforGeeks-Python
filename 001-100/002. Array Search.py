ed24a330b6072474a2c313edc66a572899de6dbc
Ravi Kiran B Y <ravikiranby02@gmail.com>
2026-03-11 00:14:56 +0530

    Update and rename 002.py to 002. Array Search.py

--- 001-100/002. Array Search.py
+++ 001-100/002. Array Search.py
@@ -1,9 +1,8 @@
-"""Array Search"""
-
 class Solution:
     def search(self, arr, x):
         
         for index in range(len(arr)):
             if arr[index] == x:
                 return index
+
         return -1
