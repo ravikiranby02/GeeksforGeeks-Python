e10a5a135ba76be60881a11f549c5186d1790462
Ravi Kiran B Y <ravikiranby02@gmail.com>
2026-03-12 00:42:33 +0530

    Update and rename 006.py to 006. Min and Max in Array.py

--- 001-100/006. Min and Max in Array.py
+++ 001-100/006. Min and Max in Array.py
@@ -1,9 +1,8 @@
-"""Min and Max in Array"""
-
 class Solution:
     def getMinMax(self, arr):
         arr.sort()
         k = []
         k.append(arr[0])
         k.append(arr[-1])
+
         return k
