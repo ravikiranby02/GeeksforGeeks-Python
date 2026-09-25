07f56139b10352acb8188fe5cd1d0b7e8a4150cb
RAVI KIRAN B Y <ravikiranby02@gmail.com>
2026-09-25 07:58:52 +0530

    Update 021. 1 to n Without Loops.py

--- 001-100/021. 1 to n Without Loops.py
+++ 001-100/021. 1 to n Without Loops.py
@@ -2,5 +2,6 @@ class Solution:
     def printTillN(self, n, current=1):
         if current > n:
             return
+            
         print(current, end=" ")
         self.printTillN(n, current + 1)
